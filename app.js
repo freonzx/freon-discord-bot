00935f206076fbadc71a1fd213cd0099f2d1e5af
Yago Theodoro <freonzx@gmail.com>
2018-05-03 08:58:09 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const btcValue = require('btc-value');
+const monero = require('monero');
+
+var bot = new Discord.Client();
+bot.on("ready", function() {
+    console.log("Bot is ready...");
+    bot.user.setStatus('dnd');
+    bot.user.setActivity('At your service.');
+})
+bot.on("message", function(message) {
+    if (message.author.equals(bot.user)) return;
+    if (message.content == "!btc") {
+        btcValue().then(value => {
+            btcValue.getPercentageChangeLastDay().then(percentage => {
+                var perc = percentage
+                if (perc > 0) {
+                    message.channel.send(message.author +" O preço do  BTC é " + value + " $ \nSubiu (ultimo dia): " + perc + "% :hugging:");
+                } else if (perc < 0) {
+                    message.channel.send(message.author +" O preço do  BTC é " + value + " $ \nDesceu (ultimo dia): " + perc + "% :sob:");
+                } else {
+                    message.channel.send(message.author +" O preço do  BTC é " + value + " $ \nManteve (ultimo dia): " + perc + "% :rolling_eyes:");
+                }
+            });
+        });
+    }
+
+    if (message.content == "!xmr") {
+        monero.price(function(price) {
+            message.channel.send(message.author + " O preço do Monero é " + price + " $USD Aproximadamente R$ " + (price * 3.40));
+        })
+    }
+});
+bot.login(process.env.BOT_TOKEN);
